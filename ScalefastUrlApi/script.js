0fca3fc78071797313d87ea8e04181ba386c209e
akemi9605 <43465627+akemi9605@users.noreply.github.com>
2020-09-30 21:11:13 +0200

    Add files via upload

---
+++ ScalefastUrlApi/script.js
@@ -0,0 +1,19 @@
+function llamar() {
+    fetch('http://dummy.restapiexample.com/api/v1/employees')
+        .then(res => res.json())
+        .then(data => {
+            const array = data.data;
+            if (array.length != 0){
+                for (let i = 0; i < array.length; i++) {
+                    if (array[i].employee_salary > 100000) {
+                        let employeeSalaryLow = array[i].employee_name;
+                        let employee_salary = array[i].employee_salary;
+                        console.log(employeeSalaryLow, 'Salary:', employee_salary);
+                    }
+                }
+            }
+            
+        })
+}
+
+
