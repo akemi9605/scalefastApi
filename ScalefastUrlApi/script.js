2ce42f30c764833618acd5556e162bd45896b0b2
akemi9605 <43465627+akemi9605@users.noreply.github.com>
2020-10-01 08:46:25 +0200

    Update script.js

--- ScalefastUrlApi/script.js
+++ ScalefastUrlApi/script.js
@@ -7,15 +7,13 @@ function llamar() {
             if (array.length != 0) {
                 for (let i = 0; i < array.length; i++) {
                     if (array[i].employee_salary > 100000) {
+                        const lista = document.querySelector('#lista');
                         let employeeSalaryLow = array[i].employee_name;
                         let employee_salary = array[i].employee_salary;
+                        lista.innerHTML +=`<li>${employee_salary}<li>`
                         console.log(employeeSalaryLow, 'Salary:', employee_salary);
-
                     }
                 }
             }
         })
 }
-
-
-
